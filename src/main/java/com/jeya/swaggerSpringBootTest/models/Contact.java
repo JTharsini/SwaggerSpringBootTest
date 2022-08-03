3fd27b1a8398e2e20a51ca5bfdb0f62c813e5b5d
JJeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2022-08-03 23:52:55 +0530

    Modified api model related parameters

--- src/main/java/com/jeya/swaggerSpringBootTest/models/Contact.java
+++ src/main/java/com/jeya/swaggerSpringBootTest/models/Contact.java
@@ -1,9 +1,16 @@
 package com.jeya.swaggerSpringBootTest.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Details about the contact")
 public class Contact
 {
+  @ApiModelProperty(notes = "The unique id of the contact")
   private String id;
+  @ApiModelProperty(notes = "The person's phone number")
   private String phone;
+  @ApiModelProperty(notes = "The person's name")
   private String name;
   public String getId()
   {
